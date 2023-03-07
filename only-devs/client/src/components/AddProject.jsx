ec29fe9382fbc2ff483672b4e6961642325d0810
Khiaotic <112679225+Khiaotic@users.noreply.github.com>
2023-03-07 18:40:58 -0500

    add new project modal

--- only-devs/client/src/components/AddProject.jsx
+++ only-devs/client/src/components/AddProject.jsx
@@ -1,19 +1,34 @@
-import { Tooltip, Fab, IconButton} from "@mui/material";
-import React from "react";
-import {Add as AddIcon} from '@mui/icons-material'
+import { Tooltip, Box, TextField, Fab, Modal} from "@mui/material";
+import {React, useState} from "react";
+import AddIcon from '@mui/icons-material/Add';
 
 
 
 
 const AddProject = () => {
+    const [open, setOpen] = useState(false)
     return (
-<Tooltip title="Add" sx={{position:"fixed", bottom: 20}}>
-    {/* ////////////////BUTTON NOT SHOWING UP//////////// */}
+        <>
+<Tooltip  onClick={e=>setOpen(true)} ml={2} title="Add" sx={{position:"fixed", bottom: 20, left:{xs:"calc(50% - 25px}", md:30}}}>
+
      <Fab color='#F2B749' aria-label="add">
        <AddIcon/>
         </Fab>
     </Tooltip>
-    );
-};
+    
+    <Modal
+  open={open}
+  onClose={e=>setOpen(false)}
+  aria-labelledby="modal-modal-title"
+  aria-describedby="modal-modal-description"
+>
+  <Box >
+   
+  </Box>
+</Modal>
+        </>
+    )
+}
 
+///if the button is clicked or value appear return  this block of code 
 export default AddProject;
