182c0f66947650e5fa2267f7d3fce1b1860ae940
Jake Sadowski <110630130+jsadowski7@users.noreply.github.com>
2023-03-06 21:20:27 -0600

    Layed out function for Signup.js page; Still need to write logic

---
+++ only-devs/client/src/pages/Signup.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+function Signup() {
+    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    };
+};
+
+export default Signup;
