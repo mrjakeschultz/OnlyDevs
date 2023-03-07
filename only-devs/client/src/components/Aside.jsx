05edfd2faabd05af197f32b49282e91f8cc1e032
Khiaotic <112679225+Khiaotic@users.noreply.github.com>
2023-03-07 14:33:37 -0500

    margin and fontweight not working in aside

--- only-devs/client/src/components/Aside.jsx
+++ only-devs/client/src/components/Aside.jsx
@@ -8,7 +8,7 @@ const Aside  = () => {
         <Box flex={2} p={2} sx={{
             display: {xs: "none", sm:'block'}}}>
                <Box position="fixed">
-<Typography variant="h6 fontWeight={100}">
+<Typography variant="h6" fontWeight={100}>
     Collaborators
 </Typography>
 <AvatarGroup max={8}>
@@ -18,6 +18,16 @@ const Aside  = () => {
   <Avatar alt="StackDatBooty" src="" />
   <Avatar alt="Steve" src="" />
 </AvatarGroup>
+
+{/* ////////DISPLAY NEW USERS THAT CREATE ACCOUNTS? */}
+
+<Typography 
+///////MARGIN and FONTWEIGHT ARE DOIN NOTHING////////
+    mt={2}
+    mb={2}
+variant="h6" fontWeight={100}>
+    user.push(newDev)
+</Typography >
                </Box>
             </Box>
     )
