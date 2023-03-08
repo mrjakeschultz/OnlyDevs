7044a4bb31ed7ce765429134e0bcc6bcc6aa64a3
mrjakeschultz <111388769+mrjakeschultz@users.noreply.github.com>
2023-03-07 21:12:21 -0600

    add getprojects code and defs/queries

--- only-devs/client/src/Pages/Project/BrowseProjects.js
+++ only-devs/client/src/Pages/Project/BrowseProjects.js
@@ -1,37 +1,49 @@
-import React from 'react';
-import Projects from 'Project';
+import React from "react";
+import Projects from "Project";
+import { useQuery } from "@apollo/client";
+import { ProjectListing } from "../../components";
+import { typeDefs } from "../../schemas";
+
+const BrowseProjects = () => {
+	const { loading, error, data } = useQuery(typeDefs.GetProjects);
+
+	if (loading) return <p>Loading...</p>;
+	if (error) return <p>Error: {error.message}</p>;
+
+	return <ProjectListing projects={data.projects} />;
+};
 
 const ProjectListing = ({ projects }) => {
-  return (
-    <div className="project-listing">
-      <h1>Projects</h1>
-      <ul className="project-list">
-        {projects.map(project => (
-          <li key={project.id} className="project-item">
-            <div className="project-info">
-              <h2 className="project-name">{project.name}name</h2>
-              <p className="project-description">{project.description}description</p>
-              <p className="project-author">{project.author}author</p>
-              <p className="project-skillsNeeded">{project.skillsNeeded}skillsNeeded</p>
-            </div>
-            <button className="collaborate-button">Collaborate!</button>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+	return (
+		<div className="project-listing">
+			<h1>Projects</h1>
+			<ul className="project-list">
+				{projects.map((project) => (
+					<li key={project.id} className="project-item">
+						<div className="project-info">
+							<h2 className="project-name">{project.name}</h2>
+							<p className="project-description">{project.description}</p>
+							<p className="project-author">{project.author.name}</p>
+							<p className="project-skillsNeeded">{project.skillsNeeded}</p>
+						</div>
+						<button className="collaborate-button">Collaborate!</button>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 };
 
 ProjectListing.propTypes = {
-  projects: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      description: PropTypes.string.isRequired,
-      author: PropTypes.string.isRequired,
-      skillsNeeded: PropTypes.string.isRequired,
-    })
-  ),
+	projects: PropTypes.arrayOf(
+		PropTypes.shape({
+			id: PropTypes.string.isRequired,
+			name: PropTypes.string.isRequired,
+			description: PropTypes.string.isRequired,
+			author: PropTypes.string.isRequired,
+			skillsNeeded: PropTypes.string.isRequired,
+		})
+	),
 };
 
-export default ProjectListing;
+export default BrowseProjects;
--- only-devs/server/schemas/typeDefs.js
+++ only-devs/server/schemas/typeDefs.js
@@ -1,27 +1,40 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-    type User {
-        _id: ID!
-        username: String!
-        password: String!
-        email: String!
-        skills: String!
-    }
-    
-    type Project {
-        id: ID!
-        name: String!
-        description: String!
-        author: String!
-        skillsNeeded: String!
-    }
+	type User {
+		_id: ID!
+		username: String!
+		password: String!
+		email: String!
+		skills: String!
+	}
 
-    type Query {
-        user: [User]
-        project: [Project]
-        me: User
-    }
-    `;
+	type Project {
+		id: ID!
+		name: String!
+		description: String!
+		author: String!
+		skillsNeeded: String!
+	}
+
+	type Query {
+		user: [User]
+		project: [Project]
+		me: User
+	}
+
+	query GetProjects {
+		projects {
+			id
+			name
+			description
+			author {
+				id
+				name
+			}
+			skillsNeeded
+		}
+	}
+`;
 
 module.exports = typeDefs;
